30cb25737be2dc056bbae997969a74992f046175
Masaki Koyanagi <mascii@gmail.com>
2017-05-02 21:19:37 +0900

    fix loop

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import time
 import wiringpi as w 
 w.wiringPiSetup()
 w.pinMode(0,1)
-w.digitalWrite(0,1)
-time.sleep(1)
-w.digitalWrite(0,0)
+while 1:
+  w.digitalWrite(0,1)
+  time.sleep(1)
+  w.digitalWrite(0,0)
+  time.sleep(1)
