7d99553618b6e8e94354ac2c87743d449c2ac9ef
Masaki Koyanagi <mascii@gmail.com>
2017-05-02 21:30:02 +0900

    change values

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(47, GPIO.OUT)
 
-for i in range(20):
+for i in range(1000):
     GPIO.output(47, GPIO.LOW)
-    time.sleep(0.8)
+    time.sleep(0.2)
     GPIO.output(47, GPIO.HIGH)
     time.sleep(0.2)
 
